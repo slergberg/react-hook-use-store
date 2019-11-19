85ea4a46679e781380e8d0575c26f62502ad20a3
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2019-11-19 11:14:07 -0300

    Tweak build script config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
 import { terser } from 'rollup-plugin-terser'
-import pkg from './package.json'
 
-const env = process.env.NODE_ENV
+import packageDefinition from './package.json'
+
+const { NODE_ENV } = process.env
 
 const config = {
   input: 'src/index.js',
-  external: Object.keys(pkg.peerDependencies || {}),
+  external: Object.keys(packageDefinition.peerDependencies || {}),
   output: {
     format: 'umd',
     name: 'ReactHookUseStore',
@@ -24,13 +25,13 @@ const config = {
       runtimeHelpers: true,
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify(env),
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
     }),
     commonjs(),
   ],
 }
 
-if (env === 'production') {
+if (NODE_ENV === 'production') {
   config.plugins.push(
     terser({
       compress: {
